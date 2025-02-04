80659898b0024042ec3ef43a4cc431f894484787
Jonathan Morris <jonathanmorris678@gmail.com>
2025-02-03 22:15:30 -0500

    Fix tmux issue

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -15,7 +15,18 @@ config.window_padding = {
 	top = 5,
 	bottom = 5,
 }
+config.term = "tmux-256color" -- this has to be set for tmux's Ctrl-Space + [ to work
+-- config.term = "xterm-kitty" -- might be needed for images to work, see https://www.youtube.com/watch?v=8m88Mh12yVw&t=121s
+
+-- To enable kitty graphics
+-- https://github.com/wez/wezterm/issues/986
+-- It seems that kitty graphics is not enabled by default according to this
+-- Not sure, so I'm enabling it just in case
+-- https://github.com/wez/wezterm/issues/1406#issuecomment-996253377
+config.enable_kitty_graphics = true
 
 config.window_decorations = "TITLE | RESIZE"
+config.window_close_confirmation = "NeverPrompt"
+config.max_fps = 120
 
 return config
